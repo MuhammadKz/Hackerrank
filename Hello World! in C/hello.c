f48019170d1351869bcd87223adf38caa9127b71
MuhammadKz <kazimuhammad0104@gmail.com>
2024-11-21 20:25:46 +0600

    hackerrank code

---
+++ Hello World! in C/hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main() {
+    char inputString[100]; 
+
+    
+    scanf("%[^\n]", inputString);
+
+    printf("Hello, World!\n");
+
+    printf("%s\n", inputString);
+
+    return 0;
+}
+
